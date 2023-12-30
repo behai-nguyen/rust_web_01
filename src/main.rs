d6d60d7d2e2bac2a0f1a626df50c0c24575dfd53
behai-nguyen <behai_nguyen@hotmail.com>
2023-12-31 10:35:00 +1100

    Integration tests
    Bind to port 5000 instead of dynamic allocation.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use learn_actix_web::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("0.0.0.0:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("0.0.0.0:5000").expect("Failed to bind port 5000");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     println!("Server is listening on port {}", port);
