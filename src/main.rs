dd0967f0c3a70d9015919b91c1559bdc617dc44a
behai-nguyen <behai_nguyen@hotmail.com>
2023-12-30 21:41:47 +1100

    Integration tests
    Completely refactored: all codes moved to src/lib.rs. Now just calls run() method from src/lib.rs.

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,15 @@
 
 //! Web application entry function.
 
-use dotenv::dotenv;
-use sqlx::{Pool, MySql};
-use async_std::task;
-use actix_web::{http::header, web, App, HttpServer};
-use actix_cors::Cors;
-
-mod config;
-mod database;
-mod utils;
-mod models;
-mod handlers;
-
-mod middleware;
-
-pub struct AppState {
-    db: Pool<MySql>,
-}
+use std::net::TcpListener;
+use learn_actix_web::run;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-    let config = config::Config::init();
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("0.0.0.0:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    println!("Server is listening on port {}", port);
 
-    let pool = task::block_on(database::get_mysql_pool(config.max_connections, &config.database_url));
-
-    HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin(&config.allowed_origin)
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![
-                header::CONTENT_TYPE,
-                header::AUTHORIZATION,
-                header::ACCEPT,
-            ])
-            .max_age(config.max_age)
-            .supports_credentials();
-
-        App::new()
-            .app_data(web::Data::new(AppState {
-                db: pool.clone()
-            }))
-            .wrap(cors)
-            .service(
-                web::scope("/data")
-                    .service(handlers::employees_json1)
-                    .service(handlers::employees_json2),
-            )
-            .service(
-                web::scope("/ui")
-                    .service(handlers::employees_html1)
-                    .service(handlers::employees_html2),
-            )
-            .service(
-                web::resource("/helloemployee/{last_name}/{first_name}")
-                    .wrap(middleware::SayHi)
-                    .route(web::get().to(handlers::hi_first_employee_found))
-            )
-    })
-    .bind(("0.0.0.0", 5000))?
-    .run()
-    .await
+    run(listener)?.await
 }
