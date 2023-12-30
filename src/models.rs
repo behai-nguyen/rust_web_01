d1ea45725012e6f86ca8d01e0c701e7c5af58a79
behai-nguyen <behai_nguyen@hotmail.com>
2023-12-30 21:42:08 +1100

    Integration tests
    Bug fixed doc-tests. Employee now implements Deserialize. Added unit tests.

--- src/models.rs
+++ src/models.rs
@@ -14,7 +14,7 @@ use crate::utils;
 /// Represents the ``employees`` table in the database. Values of [`sqlx::types::time::Date`] 
 /// fields are converted into Australian date format ``dd/mm/yyyy`` before
 /// sending back to the client.
-#[derive(FromRow, Serialize)]
+#[derive(FromRow, Debug, Deserialize, Serialize)]
 pub struct Employee {
     pub emp_no: i32,
     #[serde(with = "utils::australian_date_format")]
@@ -67,13 +67,12 @@ pub struct EmployeeSearch {
 /// ```
 /// use async_std::task;
 /// 
-/// mod database;
-/// mod models;
+/// use learn_actix_web::database;
+/// use learn_actix_web::models::get_employees;
 /// 
-/// use models::get_employees;
 /// //..
 /// let pool = task::block_on(database::get_mysql_pool(5, "mysql://root:pcb.2176310315865259@localhost:3306/employees"));
-/// let query_result = task::block_on(get_employees(pool, "nguy%", "be%"));
+/// let query_result = task::block_on(get_employees(&pool, "nguy%", "be%"));
 /// ```
 pub async fn get_employees(
     pool: &Pool<MySql>,
@@ -93,4 +92,48 @@ pub async fn get_employees(
         }
     })
     .fetch_all(pool).await.unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use time::macros::date;
+    use super::*;
+
+    #[test]
+    fn test_employee_serde() {
+        let json_str = r#"{
+            "emp_no": 67115,
+            "birth_date": "14/12/1955",
+            "first_name": "Siamak",
+            "last_name": "Bernardeschi",
+            "gender": "M",
+            "hire_date": "26/04/1985"
+        }"#;
+    
+        let emp: Employee = serde_json::from_str(json_str).unwrap();
+        assert_eq!(emp.birth_date, date!(1955 - 12 - 14));
+        assert_eq!(emp.hire_date, date!(1985 - 04 - 26));
+    
+        let expected_str = String::from("{\n  \"emp_no\": 67115,\n  \"birth_date\": \"14/12/1955\",\n  \"first_name\": \"Siamak\",\n  \"last_name\": \"Bernardeschi\",\n  \"gender\": \"M\",\n  \"hire_date\": \"26/04/1985\"\n}");
+        let serialized = serde_json::to_string_pretty(&emp).unwrap();
+        assert_eq!(serialized, expected_str);
+    }
+
+    #[test]
+    fn test_employee_serde_failure() {
+        let json_str = r#"{
+            "emp_no": 67115,
+            "birth_date": "30/02/1955",
+            "first_name": "Siamak",
+            "last_name": "Bernardeschi",
+            "gender": "M",
+            "hire_date": "26/04/1985"
+        }"#;
+    
+        match serde_json::from_str::<Employee>(json_str) {
+            Ok(_emp) => assert!(true == false, "Expect deserialisation error."),
+            Err(err) => assert!(err.to_string()
+                .contains("Error deserialise 30/02/1955 to YYYY-MM-DD"), "Expect birth_date deserialisation error"),
+        };
+    }    
 }
