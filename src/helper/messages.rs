1469f01875b05705e5eafda61e7e2883da7ecad9
behai-nguyen <behai_nguyen@hotmail.com>
2024-01-26 23:49:06 +1100

    Email and password login. Request authentication.
    Application messages.

--- src/helper/messages.rs
+++ src/helper/messages.rs
@@ -5,3 +5,4 @@
 pub static CONTENT_TYPE_NOT_RECOGNISED_MSG: &str = "Content type not recognised.";
 pub static REQUEST_BODY_EMPTY_MSG: &str = "Request body is empty.";
 pub static LOGIN_FAILURE_MSG: &str = "Please check login detail.";
+pub static UNAUTHORISED_ACCESS_MSG: &str = "Please log in first.";
