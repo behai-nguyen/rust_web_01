e8b27f17f28dad755a414ec915695cc7c4d65957
behai-nguyen <behai_nguyen@hotmail.com>
2024-01-26 23:48:49 +1100

    Email and password login. Request authentication.
    Define some constants for names.

---
+++ src/helper/constants.rs
@@ -0,0 +1,6 @@
+/* 10/01/2024. */
+
+//! Application specific constants.
+
+pub static REDIRECT_MESSAGE: &str = "redirect-message";
+pub static ORIGINAL_CONTENT_TYPE: &str = "original-content-type";
