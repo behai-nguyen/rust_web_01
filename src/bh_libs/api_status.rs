6fbbab2d12d5a5c9b0da237b28b2ef2692f8b042
behai-nguyen <behai_nguyen@hotmail.com>
2024-01-26 23:50:42 +1100

    Email and password login. Request authentication.
    All fields are now private.
    'text' field renamed to 'message'.
    Implemented get_xxx methods.

--- src/bh_libs/api_status.rs
+++ src/bh_libs/api_status.rs
@@ -6,20 +6,24 @@
 //! It is modelled after [Python bh-apistatus package](https://bh-apistatus.readthedocs.io/en/latest/).
 //! 
 
+// Command to run doc-tests: 
+// 
+//     * cargo test --doc bh_libs::api_status
+//
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
 // Struct actix_web::http::StatusCode / https://docs.rs/actix-web/latest/actix_web/http/struct.StatusCode.html
 
 /// Encapsulate result status of server operations in response to client requests.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ApiStatus {
     /// One of those HTTP codes defined in [`actix_web::http::StatusCode`].
-    pub code: u16,
+    code: u16,
     /// An optional message.
-    pub text: Option<String>,
+    message: Option<String>,
     /// An optional web session identifier.
-    pub session_id: Option<String>,
+    session_id: Option<String>,
 }
 
 /// For debugging etc., display [`ApiStatus`] as JSON.
@@ -29,7 +33,6 @@ impl fmt::Display for ApiStatus {
     }
 }
 
-// Command to run doc-tests: cargo test --doc bh_libs::api_status
 impl ApiStatus {
     /// Returns an API status with the given code.
     /// 
@@ -46,7 +49,12 @@ impl ApiStatus {
     /// let status = ApiStatus::new(StatusCode::INTERNAL_SERVER_ERROR.as_u16());
     /// ```
     pub fn new(code: u16) -> Self {
-        Self { code, text: None, session_id: None }
+        Self { code, message: None, session_id: None }
+    }
+
+    /// Returns the value of the ``code`` field.
+    pub fn get_code(&self) -> u16 {
+        self.code
     }
 
     /// Sets the `text` field on `self`, and returns itself.
@@ -62,11 +70,16 @@ impl ApiStatus {
     /// use learn_actix_web::bh_libs::api_status::ApiStatus;
     /// 
     /// let status = ApiStatus::new(StatusCode::INTERNAL_SERVER_ERROR.as_u16())
-    ///     .set_text("I am testing...");
+    ///     .set_message("I am testing...");
     /// ```
-    pub fn set_text(mut self, text: &str) -> Self {
-        self.text = Some(String::from(text));
+    pub fn set_message(mut self, text: &str) -> Self {
+        self.message = Some(String::from(text));
         self
+    }
+
+    /// Returns the value of the ``message`` field.
+    pub fn get_message(&self) -> Option<String> {
+        self.message.clone()
     }
 
     /// Sets the `text` field on `self`, and returns itself.
@@ -88,4 +101,9 @@ impl ApiStatus {
         self.session_id = Some(String::from(session_id));
         self
     }
+
+    /// Returns the value of the ``session_id`` field.
+    pub fn get_session_id(&self) -> Option<String> {
+        self.session_id.clone()
+    }    
 }
