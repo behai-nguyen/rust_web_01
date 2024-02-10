5f48cc96939079e82f38fb621658aeee3a18c668
behai-nguyen <behai_nguyen@hotmail.com>
2024-02-10 15:17:45 +1100

    Implement SSL/HTTPS, i.e. https://localhost:5000/api/login.
    Modified pub async fn spawn_app() -> TestApp app_url scheme changed to https.
    Added pub fn reqwest_client() -> reqwest::Client -- integration test methods must call this.
    assert_json_login_failure renamed to assert_json_failure.

--- tests/common.rs
+++ tests/common.rs
@@ -2,9 +2,11 @@
 
 //! Light common functions in used in tests.
 
+use std::{fs::File, io::Read as _,};
 use std::net::TcpListener;
 
 use actix_web::http::{StatusCode, header};
+use reqwest::tls::Certificate;
 
 use learn_actix_web::bh_libs::api_status::ApiStatus;
 use learn_actix_web::run;
@@ -33,8 +35,71 @@ pub async fn spawn_app() -> TestApp {
     let _ = tokio::spawn(server);
 
     TestApp {
-        app_url: format!("http://127.0.0.1:{}", port)
+        app_url: format!("https://127.0.0.1:{}", port)
     }
+}
+
+/// See https://github.com/actix/examples/tree/master/https-tls/openssl
+/// https://docs.rs/reqwest/latest/reqwest/tls/struct.Certificate.html
+///  
+/// This certificate has been generated on Ubuntu 22.10. 
+///
+/// It seems that ``.add_root_certificate(load_certificate())`` only 
+/// required on the first run? Without it, certicate fails with:
+///
+/// ```
+/// Failed to execute request.: reqwest::Error { kind: Request, url: Url { scheme: "https", 
+///     cannot_be_a_base: false, username: "", password: None, host: Some(Ipv4(127.0.0.1)), 
+///     port: Some(36159), path: "/api/login", query: None, fragment: None }, 
+///     source: hyper::Error(Connect, Ssl(Error { code: ErrorCode(1), cause: 
+///     Some(Ssl(ErrorStack([Error { code: 167772294, library: "SSL routines", 
+///     function: "tls_post_process_server_certificate", reason: "certificate verify failed", 
+///     file: "../ssl/statem/statem_clnt.c", line: 1889 }]))) }, X509VerifyResult { code: 18, 
+///     error: "self-signed certificate" })) }
+/// ```
+///
+/// After the first run (?), ``.add_root_certificate(load_certificate())`` can be removed?
+///
+pub fn load_certificate() -> Certificate {
+    let mut buf = Vec::new();
+    File::open("./cert/cert-pass.pem")
+        .unwrap()
+        .read_to_end(&mut buf).unwrap();
+
+    Certificate::from_pem(&buf).unwrap()
+}
+
+/// Creates a reqwest::Client to work with the HTTPS server.
+/// There is no need to load the certificate that the server is using.
+/// 
+/// # References
+/// 
+///    https://stackoverflow.com/questions/76724036/how-to-resolve-a-rust-reqwest-error-invalid-certificate
+///    Answer https://stackoverflow.com/questions/76724036/how-to-resolve-a-rust-reqwest-error-invalid-certificate#comment135281495_76724036
+/// 
+/// mentions using ``.danger_accept_invalid_cert(true)``.
+/// 
+/// Without calling ``.danger_accept_invalid_certs(true)`` -- note the s -- it would
+/// result in the following error:
+/// 
+/// ```
+/// Failed to execute request.: reqwest::Error { kind: Request, url: Url { scheme: "https", cannot_be_a_base: false, username: "", password: None, host: Some(Ipv4(127.0.0.1)), port: Some(62730), path: "/ui/home", query: None, fragment: None }, source: hyper::Error(Connect, Os { code: -2146762487, kind: Uncategorized, message: "A certificate chain processed, but terminated in a root certificate which is not trusted by the trust provider." }) }
+/// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+/// ```
+///
+/// or
+/// 
+/// ```
+/// Failed to execute request.: reqwest::Error { kind: Request, url: Url { scheme: "http", cannot_be_a_base: false, username: "", password: None, host: Some(Ipv4(127.0.0.1)), port: Some(65030), path: "/api/login", query: None, fragment: None }, source: hyper::Error(Io, Os { code: 10054, kind: ConnectionReset, message: "An existing connection was forcibly closed by the remote host." }) }
+/// ```
+/// 
+pub fn reqwest_client() -> reqwest::Client {
+    reqwest::Client::builder()
+        .add_root_certificate(load_certificate())
+        .danger_accept_invalid_certs(true) 
+        .cookie_store(true)
+        .build()
+        .unwrap()
 }
 
 pub fn make_full_url(root: &str, path: &str) -> String {
@@ -159,7 +224,7 @@ pub async fn assert_redirected_html_login_page(
     }    
 }
 
-pub async fn assert_json_login_failure(
+pub async fn assert_json_failure(
     response: reqwest::Response,
     status_code: StatusCode,
     reason: &str,
