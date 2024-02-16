897024f0ca2c8a3a6acdc55bdbb054f350ec6877
behai-nguyen <behai_nguyen@hotmail.com>
2024-02-17 10:53:09 +1100

    Implemented global extractor error handlers.
    Added pub fn serialise_api_status(...)
    Removed pub fn extract_employee_login(...)

--- src/helper/endpoint.rs
+++ src/helper/endpoint.rs
@@ -2,19 +2,12 @@
 
 //! Application request handler helper methods. **Work in progress**.
 
-use actix_web::web::Bytes;
 use actix_web::http::StatusCode;
 
 use serde_json;
 
-use serde_html_form::from_bytes;
-
-use crate::models::{EmployeeLogin, LoginSuccess, LoginSuccessResponse};
+use crate::models::{LoginSuccess, LoginSuccessResponse};
 use crate::bh_libs::api_status::ApiStatus;
-use crate::helper::messages::{
-    REQUEST_BODY_EMPTY_MSG,
-    CONTENT_TYPE_NOT_RECOGNISED_MSG
-};
 
 /// Returns an [`actix_web::http::StatusCode`] as an [`u16`].
 /// 
@@ -22,98 +15,26 @@ pub fn http_status_code(status_code: StatusCode) -> u16 {
     status_code.as_u16()
 }
 
-/// Attempts to deserialise user submitted employee login information into struct
-/// [`EmployeeLogin`]. The submitted data can be in [`mime::APPLICATION_WWW_FORM_URLENCODED`]
-/// or [`mime::APPLICATION_JSON`]. Any other content types are invalid.
-/// 
-/// *Note, the objective is to turn this method into generic later on. I've attempted, and failed.*
-/// 
-/// # Arguments
-/// 
-/// * `body` - [`actix_web::web::Bytes`], the actual submitted data.
-/// 
-/// * `content_type` - a string slice [`str`]. Valid values are 
-/// [`mime::APPLICATION_WWW_FORM_URLENCODED`] and [`mime::APPLICATION_JSON`].
+/// Creates and returns a serialised version of [`ApiStatus`].
 /// 
 /// # Return
 /// 
-/// - [`std::result::Result`]&lt;[`EmployeeLogin`], [`ApiStatus`]&gt; - that is,
-/// returns [`EmployeeLogin`] if deserialisation (extraction) is successful, 
-/// [`ApiStatus`] otherwise.
+/// * JSON string of [`ApiStatus`].
 /// 
-/// # Usage Example
-/// 
-/// * See [`crate::auth_handlers::login`]'s implementation, i.e., actual code.
-/// 
-pub fn extract_employee_login(
-    body: &Bytes, 
-    content_type: &str
-) -> Result<EmployeeLogin, ApiStatus> {
-    // No content. Returns an error.
-    if body.len() == 0  {
-        return Err(
-            ApiStatus::new(http_status_code(StatusCode::BAD_REQUEST))
-                .set_message(REQUEST_BODY_EMPTY_MSG)
-        );
+pub fn serialise_api_status(
+    status_code: StatusCode,
+    message: &str,
+    session_id: Option<String>
+) -> String {
+    let mut api_status = ApiStatus::new(http_status_code(status_code))
+        .set_message(message);
+
+    if let Some(sess_id) = session_id {
+        api_status = api_status.set_session_id(&sess_id);
     }
 
-    // Content type and associated extraction function.
-    struct Extractor {
-        content_type: String,
-        handler: fn(body: &Bytes) -> Result<EmployeeLogin, ApiStatus>,
-    }
-
-    // A list of Extractor.
-    let mut extractors: Vec<Extractor> = vec![];
-
-    // "application/x-www-form-urlencoded" content type and extraction function. 
-    // That is, a function which deserialises a byte stream presentation of 
-    // "application/x-www-form-urlencoded" to EmployeeLogin.
-    extractors.push(Extractor { 
-        content_type: mime::APPLICATION_WWW_FORM_URLENCODED.to_string(), 
-        handler: |body: &Bytes| -> Result<EmployeeLogin, ApiStatus> {
-            match from_bytes::<EmployeeLogin>(&body.to_owned().to_vec()) {
-                Ok(e) => Ok(e),
-                Err(e) => Err(
-                    ApiStatus::new(http_status_code(StatusCode::BAD_REQUEST))
-                        .set_message(&e.to_string())
-                )
-            }
-        }
-    });
-
-    // "application/json" content type and extraction function. That is, a function which 
-    // deserialises a byte stream presentation of "application/json" to EmployeeLogin.
-    extractors.push(Extractor {
-        content_type: mime::APPLICATION_JSON.to_string(),
-        handler: |body: &Bytes| -> Result<EmployeeLogin, ApiStatus> {
-            // From https://stackoverflow.com/a/67340858
-            match serde_json::from_slice(&body.to_owned()) {
-                Ok(e) => Ok(e),
-                Err(e) => Err(
-                    ApiStatus::new(http_status_code(StatusCode::BAD_REQUEST))
-                        .set_message(&e.to_string())
-                )
-            }
-        }
-    });
-
-    // If param content_type is a recognised content type, then attempt to 
-    // deserialise the byte stream.
-    for extractor in extractors {
-        if extractor.content_type == content_type {
-            return (extractor.handler)(body);
-        }
-    }
-
-    // Param content_type is not recognised, return ApiStatus with an 
-    // appropriate message.
-    Err(
-        ApiStatus::new(http_status_code(StatusCode::BAD_REQUEST))
-            .set_message(CONTENT_TYPE_NOT_RECOGNISED_MSG)
-    )
+    serde_json::to_string(&api_status).unwrap()
 }
-
 
 /// Constructs a successful login JSON response.
 ///
