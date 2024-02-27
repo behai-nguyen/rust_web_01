526c7f4d41711ab316faa5e4900379b97bd0b182
behai-nguyen <behai_nguyen@hotmail.com>
2024-02-27 11:00:40 +1100

    Implemented proper JSON Web Token using jsonwebtoken crate.
    Added TOKEN_INVALID_MSG, TOKEN_EXPIRED_MSG, TOKEN_OTHER_ERR_MSG, TOKEN_STR_JWT_MSG.

--- src/helper/messages.rs
+++ src/helper/messages.rs
@@ -4,3 +4,7 @@
 
 pub static LOGIN_FAILURE_MSG: &str = "Please check login detail.";
 pub static UNAUTHORISED_ACCESS_MSG: &str = "Please log in first.";
+pub static TOKEN_INVALID_MSG: &str = "Invalid token.";
+pub static TOKEN_EXPIRED_MSG: &str = "Token has expired.";
+pub static TOKEN_OTHER_ERR_MSG: &str = "Token is in error.";
+pub static TOKEN_STR_JWT_MSG: &str = "JWT extension should be a string.";
